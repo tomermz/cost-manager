1634c21c41dfb94ffdd5f4f32c4911a72134096c
tomermz <tomermz20@gmail.com>
2025-09-15 21:07:39 +0300

    init commit

--- tests/test1.js
+++ tests/test1.js
@@ -1,34 +1,23 @@
-import idb from "public/idb.js";
-
-async function test() {
-
-    const db = await idb.openCostsDB("costsdb",1);
-
-    const result1 =
-        await db.addCost({ sum: 200, currency: "USD", category: "FOOD", description: "pizza" });
-
-    const result2 =
-        await db.addCost({ sum: 400, currency: "USD", category: "CAR", description: "fuel" });
-
-
-    if(db) {
-
-        console.log("creating db succeeded");
-
-    }
-
-    if(result1) {
-
-        console.log("adding 1st cost item succeeded");
-
-    }
-
-    if(result2) {
-
-        console.log("adding 2nd cost item succeeded");
-
-    }
-
+// Function to log messages both to the console and on the page
+function log(msg) {
+    console.log(msg);
+    const p = document.createElement("p");
+    p.textContent = msg;
+    document.body.appendChild(p);
 }
 
-test()
+// Function to run the test
+async function test() {
+    // Assuming idb.js exports an object named 'idb'
+    const db = await idb.openCostsDB("costsdb", 1);
+
+    const result1 = await db.addCost({ sum: 200, currency: "USD", category: "FOOD", description: "pizza" });
+    const result2 = await db.addCost({ sum: 400, currency: "USD", category: "CAR", description: "fuel" });
+
+    if(db) log("Database creation succeeded");
+    if(result1) log("Adding first cost item succeeded");
+    if(result2) log("Adding second cost item succeeded");
+}
+
+// Run the test
+test();
